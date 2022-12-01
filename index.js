2843c3fde547ef080ba5a34d89f52849ff9134a6
Sylvia Ely <sylvianne9417@gmail.com>
2022-12-01 14:19:04 -0800

    adding when as a conditional to inquirer questions

--- index.js
+++ index.js
@@ -9,76 +9,68 @@ const engineer = require("./lib/engineer");
 const intern = require("./lib/intern");
 
 //require html file that we will add names to
-const htmlGenerate = require("./dist/index.html");
+//const htmlGenerate = require("./dist/index.html");
+//const { getSystemErrorName } = require("util");
 
-//function that will create the actual markdown doc with user generated information
-function writeToFile(README, renderedMarkdown) {
-  fs.writeFile(README, renderedMarkdown, (err) =>
-    err ? console.error(err) : console.log("README generated")
-  );
-}
+//function that will create the actual html???  doc with user generated information??
+// function writeToHTML???(README, renderedMarkdown) {
+//   fs.writeFile(README, renderedMarkdown, (err) =>
+//     err ? console.error(err) : console.log("README generated")
+//   );
 
 //initialize project.  Asks questions in the terminal
 function init() {
   inquirer
     .prompt([
       {
-        type: "input",
-        message: questions[0],
-        name: "questionsTitle",
-      },
-      {
-        type: "input",
-        message: questions[1],
-        name: "questionsDescr",
-      },
-      {
-        type: "input",
-        message: questions[2],
-        name: "questionsInstall",
-      },
-      {
-        type: "input",
-        message: questions[3],
-        name: "questionsUsage",
+        type: "list",
+        name: "addEmployee",
+        message: "Would you like to add an employee?",
+        choices: ["yes", "no"],
       },
       {
         type: "list",
-        message: questions[4],
-        choices: [
-          "MIT License",
-          "ISC License",
-          "Apache License 2.0",
-          "GNU General Public License v3.0",
-        ],
-        name: "questionsLicense",
+        name: "position",
+        message: "What type of employee would you like to add",
+        choices: ["Manager", "Engineer", "Intern"],
+        when(answers) {
+          return answers.addEmployee === "yes";
+        },
       },
+
       {
         type: "input",
-        message: questions[5],
-        name: "questionsContribution",
+        name: "officeNumber",
+        message: "What is the manager's office number?",
+        when(answers) {
+          return answers.position === "Manager";
+        },
       },
+
       {
         type: "input",
-        message: questions[6],
-        name: "questionsTesting",
+        name: "gitHub",
+        message: "What is the engineer's github id?",
+        when(answers) {
+          return answers.position === "Engineer";
+        },
       },
+
       {
         type: "input",
-        message: questions[7],
-        name: "questionsGitHubUser",
-      },
-      {
-        type: "input",
-        message: questions[8],
-        name: "questionsEmailAddress",
+        name: "university",
+        message: "What is the intern's school?",
+        when(answers) {
+          return answers.position === "Intern";
+        },
       },
     ])
+
     //asynchronous function that acts when a response comes back.  Uses the data to create markdown file
     .then((response) => {
-      const README = "README.md";
-      const renderedMarkdown = markdownTemplate(response);
-      writeToFile(README, renderedMarkdown);
+      const name = new employee(response);
+      //const renderedMarkdown = markdownTemplate(response);
+      //writeToFile(README, renderedMarkdown);
     });
 }
 
