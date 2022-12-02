ea68970c1c83c3fabafe66ab5d7d4907077b44de
Sylvia Ely <sylvianne9417@gmail.com>
2022-12-01 17:43:29 -0800

    trying to figure out order of inquirer questions

--- index.js
+++ index.js
@@ -24,15 +24,208 @@ function init() {
     .prompt([
       {
         type: "list",
-        name: "addEmployee",
-        message: "Would you like to add an employee?",
+        name: "addManager",
+        message: "Would you like to add the manager?",
         choices: ["yes", "no"],
       },
+    ])
+    .then((response) => {
+      if (response.addManager === "yes") {
+        const addManager = true;
+        employeeDetails();
+      }
+    });
+}
+
+function employeeDetails() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "employeeName",
+        message: "What is the employee's name?",
+      },
+
+      {
+        type: "input",
+        name: "employeeID",
+        message: "What is the employee's ID?",
+      },
+
+      {
+        type: "input",
+        name: "employeeEmail",
+        message: "What is the employee's email?",
+      },
+    ])
+    .then((response) => {
+      detailsManager();
+      return response;
+    });
+}
+
+function detailsManager() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "managerOffice",
+        message: "What is the manager's office number?",
+      },
+    ])
+    .then((response) => {
+      addAdditionalEmployee();
+      return response;
+    });
+}
+
+function addAdditionalEmployee() {
+  inquirer
+    .prompt([
       {
         type: "list",
-        name: "position",
-        message: "What type of employee would you like to add",
-        choices: ["Manager", "Engineer", "Intern"],
+        name: "addEmployee",
+        message: "Would you like to add an additional Employee?",
+        choices: ["yes", "no"],
+      },
+    ])
+    .then((response) => {
+      if (response.addEmployee == "yes") {
+        internOrEngineer();
+      }
+    });
+}
+
+function internOrEngineer() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "internOrEngineer",
+        message: "Would you like to add an Intern or Engineer?",
+        choices: ["Intern", "Engineer"],
+      },
+    ])
+    .then((response) => {
+      employeeDetails();
+      
+      .then(response) => {
+      if (response.internOrEngineer == "Intern") {
+        internDetails();
+      } else {
+        engineerDetails();
+      }}
+    });
+}
+
+function engineerDetails() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "engineerDetails",
+        message: "What the engineer's GitHub username?",
+      },
+    ])
+    .then((response) => {
+      addAdditionalEmployee();
+      return response;
+    });
+}
+
+function internDetails() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "internDetails",
+        message: "What the intern's university?",
+      },
+    ])
+    .then((response) => {
+      addAdditionalEmployee();
+      return response;
+    });
+}
+
+//add manager once
+//add engineer and intern together.
+
+/*
+      {
+        type: "input",
+        name: "employeeID",
+        message: "What is their employee ID?",
+        when(answers) {
+          return answers.addManager === "yes";
+        },
+      },
+
+      {
+        type: "input",
+        name: "employeeEmail",
+        message: "What is the employee's email address?",
+        when(answers) {
+          return answers.addManager === "yes";
+        },
+      },
+
+      {
+        type: "input",
+        name: "managerOffice",
+        message: "What is the manager's office number?",
+        when(answers) {
+          return answers.addManager === "yes";
+        },
+      },
+
+
+
+
+
+      {
+        type: "list",
+        name: "addManager",
+        message: "Would you like to add the manager?",
+        choices: ["yes", "no"],
+      },
+
+
+
+    ])
+
+    .then((response) => {
+      if (response.addEmployee === "yes") {
+       ;
+      }
+    });
+
+
+function employeeDetails (){
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "employeeName",
+        message: "What is the name of the employee?",
+        when(answers) {
+          return answers.addEmployee === "yes";
+        },
+      },
+
+      {
+        type: "input",
+        name: "employeeID",
+        message: "What is the employee's ID?",
+        when(answers) {
+          return answers.addEmployee === "yes";
+        },
+      },
+
+      {
+        type: "input",
+        name: "employeeEmail",
+        message: "What is the employee's email address?",
         when(answers) {
           return answers.addEmployee === "yes";
         },
@@ -64,15 +257,29 @@ function init() {
           return answers.position === "Intern";
         },
       },
+
+      {
+        type: "list",
+        name: "addAnother",
+        message: "Would you like to add another employee?",
+        choices: ["yes", "no"],
+        when(answers) {
+          return answers.addEmployee === "yes";
+        },
+      },
     ])
+
 
     //asynchronous function that acts when a response comes back.  Uses the data to create markdown file
     .then((response) => {
       const name = new employee(response);
+      //recall function? and say, user created please enter new employees information
       //const renderedMarkdown = markdownTemplate(response);
       //writeToFile(README, renderedMarkdown);
     });
 }
+}*/
 
 init();
+
 //figure out how to REQUIRE RESPONSE in node.
