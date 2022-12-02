f3c75c91110e2cbeba1b47bc125f999208686bf5
Sylvia Ely <sylvianne9417@gmail.com>
2022-12-02 13:32:33 -0800

    realize I probably need to use classes

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const inquirer = require("inquirer");
 const allEmployees = [];
 
 //require classes and subclasses
-const employee = require("./lib/employee");
-const manager = require("./lib/manager");
-const engineer = require("./lib/engineer");
-const intern = require("./lib/intern");
+const Employee = require("./lib/employee");
+const Manager = require("./lib/manager");
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
 
 //require html file that we will add names to
 //const htmlGenerate = require("./dist/index.html");
@@ -65,17 +65,14 @@ function managerDetails() {
       },
     ])
     .then((response) => {
-      let managerID = response.managerID;
-      managerID = []; //maybe want an array of objects so we can know the overarching name of the index.
-      managerID.push({
-        position: "Manager",
-        name: response.managerName,
-        id: response.managerID,
-        email: response.managerEmail,
-        officeNumber: response.managerOffice,
-      });
-      console.log(managerID);
-      allEmployees.push(managerID);
+      let manager = new Manager(
+        response.managerName,
+        response.managerID,
+        response.managerEmail,
+        response.managerOffice
+      );
+      allEmployees.push(manager);
+      console.log("Manager added.");
       console.log(allEmployees);
       addAdditionalEmployee();
       //return response;
@@ -99,6 +96,7 @@ function addAdditionalEmployee() {
         //create object holding all employee information.  for each unique array we push up to object.
 
         printArrays();
+        buildTeam();
       }
     });
 }
@@ -218,137 +216,22 @@ function printArrays() {
   console.log(allEmployees);
 }
 
+function buildTeam() {
+  fs.writeFile(`index.html`, generateHTML(allEmployees), (error) =>
+    error ? console.log(error) : console.log("HTML Build Complete")
+  );
+}
+
 //add manager once
 //add engineer and intern together.
 
-/*
-      {
-        type: "input",
-        name: "employeeID",
-        message: "What is their employee ID?",
-        when(answers) {
-          return answers.addManager === "yes";
-        },
-      },
-
-      {
-        type: "input",
-        name: "employeeEmail",
-        message: "What is the employee's email address?",
-        when(answers) {
-          return answers.addManager === "yes";
-        },
-      },
-
-      {
-        type: "input",
-        name: "managerOffice",
-        message: "What is the manager's office number?",
-        when(answers) {
-          return answers.addManager === "yes";
-        },
-      },
-
-
-
-
-
-      {
-        type: "list",
-        name: "addManager",
-        message: "Would you like to add the manager?",
-        choices: ["yes", "no"],
-      },
-
-
-
-    ])
-
-    .then((response) => {
-      if (response.addEmployee === "yes") {
-       ;
-      }
-    });
-
-
-function employeeDetails (){
-  inquirer
-    .prompt([
-      {
-        type: "input",
-        name: "employeeName",
-        message: "What is the name of the employee?",
-        when(answers) {
-          return answers.addEmployee === "yes";
-        },
-      },
-
-      {
-        type: "input",
-        name: "employeeID",
-        message: "What is the employee's ID?",
-        when(answers) {
-          return answers.addEmployee === "yes";
-        },
-      },
-
-      {
-        type: "input",
-        name: "employeeEmail",
-        message: "What is the employee's email address?",
-        when(answers) {
-          return answers.addEmployee === "yes";
-        },
-      },
-
-      {
-        type: "input",
-        name: "officeNumber",
-        message: "What is the manager's office number?",
-        when(answers) {
-          return answers.position === "Manager";
-        },
-      },
-
-      {
-        type: "input",
-        name: "gitHub",
-        message: "What is the engineer's github id?",
-        when(answers) {
-          return answers.position === "Engineer";
-        },
-      },
-
-      {
-        type: "input",
-        name: "university",
-        message: "What is the intern's school?",
-        when(answers) {
-          return answers.position === "Intern";
-        },
-      },
-
-      {
-        type: "list",
-        name: "addAnother",
-        message: "Would you like to add another employee?",
-        choices: ["yes", "no"],
-        when(answers) {
-          return answers.addEmployee === "yes";
-        },
-      },
-    ])
-
-
-    //asynchronous function that acts when a response comes back.  Uses the data to create markdown file
-    .then((response) => {
-      const name = new employee(response);
-      //recall function? and say, user created please enter new employees information
-      //const renderedMarkdown = markdownTemplate(response);
-      //writeToFile(README, renderedMarkdown);
-    });
-}
-}*/
+//asynchronous function that acts when a response comes back.  Uses the data to create markdown file
+// .then((response) => {
+//  const name = new employee(response);
+//recall function? and say, user created please enter new employees information
+//const renderedMarkdown = markdownTemplate(response);
+//writeToFile(README, renderedMarkdown);
+// });
 
 init();
 
