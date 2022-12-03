2a4ed064505d7cab7877c38997b5a1d26fee02ca
Sylvia Ely <sylvianne9417@gmail.com>
2022-12-02 16:42:35 -0800

    added more gone to generate HTML.  Runs ok but shows objectObject in index.html

--- util/generateHTML.js
+++ util/generateHTML.js
@@ -1,5 +1,8 @@
-function generateHTML(allEmployees) {
-  return `<h2 class="card-title">TESTING</h2>`;
-}
-
-module.exports = generateHTML;
+const generateTeam = (team) => {
+  console.log(team);
+  return team;
+};
+// export function to generate entire page
+module.exports = (team) => {
+  return `${generateTeam(team)}`;
+};
