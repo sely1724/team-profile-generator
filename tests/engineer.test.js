e94e2ec126fc43046de841dcb872fb6f428f2131
Sylvia Ely <sylvianne9417@gmail.com>
2022-12-04 13:07:25 -0800

    changed engineer test around to specifically look at github username instance creation and the role and username to return when new object is created

--- tests/engineer.test.js
+++ tests/engineer.test.js
@@ -1,37 +1,33 @@
 const Engineer = require("../lib/engineer");
-const Index = require("../index");
 
-//describe("Engineer", () => {
-//test for all use cases when initializing a new engineer object
-//describe("Initialization", () => {
-test("should create an object with a name, employeeID, email, and github username", () => {
+test("Ensuring GitHub User Instance is created", () => {
   const engineer = new Engineer(
-    "Engineer",
     "Dwight",
     "1234",
     "dwight@gmail.com",
     "dwight762"
   );
-  const role = "Engineer";
-  expect(engineer.getRole()).toBe(role);
-  expect(engineer.name).toBe("Dwight");
-  expect(engineer.employeeID).toBe("1234");
-  expect(engineer.email).toBe("dwight@gmail.com");
+
   expect(engineer.githubUsername).toBe("dwight762");
 });
 
-/*
+test("Ensuring user entered GitHub Username can be fetched", () => {
+  const engineer = new Engineer(
+    "Dwight",
+    "1234",
+    "dwight@gmail.com",
+    "dwight762"
+  );
+
+  expect(engineer.getGithub()).toBe("dwight762");
 });
 
-    it("should throw an error if provided no arguments", () => {
-      const callBack = () => new Engineer();
-      expect(callBack).toThrow();
-    });
-    it("should throw an error if provided fewer than 4 arguments", () => {
-      const callBack = () => new Engineer("Dwight", "1234", "dwight@gmail.com");
-      const err = new Error("Expect four arguments");
-      //ASSERT
-      expect(callBack).toThrowError(err);
-   });*/
-//});
-//});
+test("Ensuring engineer role can be fetched when new object created", () => {
+  const engineer = new Engineer(
+    "Dwight",
+    "1234",
+    "dwight@gmail.com",
+    "dwight762"
+  );
+  expect(engineer.getRole()).toBe("Engineer");
+});
